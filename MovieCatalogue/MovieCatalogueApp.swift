59df82e5a75a2ab6d814fcb88705c8a09b29db30
Rasyid <rasydridla298@gmail.com>
2021-09-15 10:53:12 +0700

    Initial Commit

---
+++ MovieCatalogue/MovieCatalogueApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieCatalogueApp.swift
+//  MovieCatalogue
+//
+//  Created by Rasyid Ridla on 15/9/21.
+//
+
+import SwiftUI
+
+@main
+struct MovieCatalogueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
