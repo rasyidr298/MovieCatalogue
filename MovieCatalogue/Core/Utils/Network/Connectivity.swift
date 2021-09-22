7c232a20c0fc4d194eac08b9e10449d181e6064a
Rasyid <rasydridla298@gmail.com>
2021-09-22 15:54:36 +0700

    fixing Connection

---
+++ MovieCatalogue/Core/Utils/Network/Connectivity.swift
@@ -0,0 +1,33 @@
+//
+//  Connectivity.swift
+//  MovieCatalogue
+//
+//  Created by Rasyid Ridla on 22/9/21.
+//
+
+import Foundation
+import SystemConfiguration
+
+// MARK: Reachability
+class Reachability {
+    public static let shared = Reachability()
+    
+    private init() { }
+    
+    func isNetworkAvailable() -> Bool {
+        let reachability = SCNetworkReachabilityCreateWithName(nil, "www.google.com")
+        var flags = SCNetworkReachabilityFlags()
+        SCNetworkReachabilityGetFlags(reachability!, &flags)
+        
+        return isNetworkReachable(with: flags)
+    }
+    
+    private func isNetworkReachable(with flags: SCNetworkReachabilityFlags) -> Bool {
+        let isReachable = flags.contains(.reachable)
+        let needsConnection = flags.contains(.connectionRequired)
+        let canConnectAutomatically = flags.contains(.connectionOnDemand) || flags.contains(.connectionOnTraffic)
+        let canConnectWithoutUserInteraction = canConnectAutomatically && !flags.contains(.interventionRequired)
+        
+        return isReachable && (!needsConnection || canConnectWithoutUserInteraction)
+    }
+}
