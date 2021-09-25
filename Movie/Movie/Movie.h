5ce408f49bef7ff77edfd23ce143c5aaf7635751
Rasyid <rasydridla298@gmail.com>
2021-09-25 11:14:32 +0700

    setup modular

---
+++ Movie/Movie/Movie.h
@@ -0,0 +1,18 @@
+//
+//  Movie.h
+//  Movie
+//
+//  Created by Rasyid Ridla on 25/9/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Movie.
+FOUNDATION_EXPORT double MovieVersionNumber;
+
+//! Project version string for Movie.
+FOUNDATION_EXPORT const unsigned char MovieVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Movie/PublicHeader.h>
+
+
---
+++ SearchMovie/SearchMovie/SearchMovie.h
@@ -0,0 +1,18 @@
+//
+//  SearchMovie.h
+//  SearchMovie
+//
+//  Created by Rasyid Ridla on 25/9/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SearchMovie.
+FOUNDATION_EXPORT double SearchMovieVersionNumber;
+
+//! Project version string for SearchMovie.
+FOUNDATION_EXPORT const unsigned char SearchMovieVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SearchMovie/PublicHeader.h>
+
+
